4d126749a37cac2e9187854bf3c8eb6e5c9d9976
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2026-05-05 19:09:39 -0400

    frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+    return (
+        <Router>
+            <div className="min-h-screen bg-gray-100">
+
+                {/* Navigation Bar */}
+                <Navbar />
+
+                <div className="p-6">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </div>
+
+            </div>
+        </Router>
+    );
+}
+
+export default App;
